bb38bec5c12222e4f1ca2d1f46d62f0bf1dcfff5
Terrance Ford <64376406+tford-dev@users.noreply.github.com>
2021-10-14 21:15:11 -0400

    Added parenthesis
    Added parenthesis to validation error handling logic

--- client/src/Components/CreateCourse.js
+++ client/src/Components/CreateCourse.js
@@ -133,11 +133,11 @@ class CreateCourse extends Component {
         };
 
         //createCourse method takes credentials from context api and course variable to execute request 
-        if (title === "" && description === "" || title === " " && description === " "){
+        if ((title === "" && description === "") || (title === " " && description === " ")){
             this.setState({errors: [...errors, "Title can not be empty!", "Description can not be empty!"]})
-        } else if (title === "" || title === " "){
+        } else if ((title === "") || (title === " ")){
             this.setState({errors: [...errors, "Title can not be empty!"]})
-        } else if (description === "" || description === " "){
+        } else if ((description === "") || (description === " ")){
             this.setState({errors: [...errors, "Description can not be empty!"]})
         } else {
             context.data.createCourse(course, authUser.emailAddress, authUser.password)
