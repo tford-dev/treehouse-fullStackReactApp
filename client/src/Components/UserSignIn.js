ccd78e10f120d43c025bbd8b33a318d7227f44f0
Terrance Ford <64376406+tford-dev@users.noreply.github.com>
2021-10-19 14:31:05 -0400

    Update UserSignIn.js

--- client/src/Components/UserSignIn.js
+++ client/src/Components/UserSignIn.js
@@ -16,7 +16,6 @@ export default class UserSignIn extends Component {
       password,
       errors,
     } = this.state;
-
     return (
         <div className="form--centered">
             <h2>Sign In</h2>
@@ -68,29 +67,34 @@ export default class UserSignIn extends Component {
 
         // {from} returns user to privateRoute that user tried to access after being authenticated or previous page before requesting to sign in
         const {from} = this.props.location.state || {from: {pathname: this.props.history.goBack()}};
-        const {emailAddress, password} = this.state;
+        const {emailAddress, password, errors} = this.state;
 
         //signIn method is grabbed from actions object that is nested in value variable in ../Context.js
-        context.actions.signIn(emailAddress, password)
-            .then(user => {
-                //If user does not exist, this.state.errors is pushed an error message that will be rendered to user
-                if(user === null){
-                    this.setState(()=>{
-                        return {errors: ["Sign-in was unsuccessful"]};
+        
+            context.actions.signIn(emailAddress, password)
+                .then((user) => {
+                    //If user does not exist, this.state.errors is pushed an error message that will be rendered to user
+                    if(user === null){
+                        this.setState(()=>{
+                            return {errors: [...errors, "Sign-In was unsuccessful."]}
+                        })
+                    //If sign in is successful, user is redirected to previous page or private route
+                    } else {
+                        this.props.history.push(from);
+                        if(window.location.pathname === "/error"){
+                            this.props.history.push("/")
+                        }
+                        console.log(`${emailAddress} is now signed in!`);
+                    }
                 })
-                //If sign in is successful, user is redirected to previous page or private route
-                } else {
-                    this.props.history.push(from);
-                    console.log(`${emailAddress} is now signed in!`);
-                }
-            })
-            .catch(err => {
-                console.log(err);
-                this.props.history.push("/error");
-            })
+                .catch(err => {
+                    console.log(err);
+                    this.props.history.push("/error");
+                })
     }
 
     cancel = () => {
         this.props.history.push('/');
+        window.location.reload();
     }
 }
